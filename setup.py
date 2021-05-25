443926dd1c7429976bcd1d5ae0acecc2ccf91401
Stefanie Unger <stefanie.unger@outlook.com>
2021-05-25 15:02:47 +0200

    add gh-actions

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='In this tutorial we learn how to use git in a software engineered project',
+    author='Stefanie',
+    license='',
+)
